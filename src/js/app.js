db586f3244190aa798015e7422acb157db541ba0
DianaCarolina3 <dcaroli2015@gmail.com>
2021-03-20 18:22:51 -0500

    ending styles and starting JavaScritp

---
+++ src/js/app.js
@@ -0,0 +1,58 @@
+
+var arrayProducts = [
+  {
+    id: 1,
+    name: 'spacex t-shirt',
+    price: 22,
+    color: 'black',
+    type: 'shirt',
+  },{
+    id: 2,
+    name: 'spacex t-shirt',
+    price: 22,
+    color: 'blue',
+    type: 'shirt',
+  },{
+    id: 3,
+    name: 'spacex t-shirt',
+    price: 22,
+    color: 'black',
+    type: 'shirt',
+  },{
+    id: 4,
+    name: 'spacex mug',
+    price: 12,
+    color: 'black',
+    type: 'mug'
+  },{
+    id: 5,
+    name: 'spacex mug',
+    price: 12,
+    color: 'black',
+    type: 'mug'
+  },{
+    id: 6,
+    name: 'spacex mug',
+    price: 12,
+    color: 'black',
+    type: 'mug'
+  },{
+    id: 7,
+    name: 'spacex cap',
+    price: 10,
+    color: 'blue',
+    type: 'cap'
+  },{
+    id: 8,
+    name: 'spacex cap',
+    price: 10,
+    color: 'black',
+    type: 'cap'
+  },{
+    id: 9,
+    name: 'spacex cap',
+    price: 10,
+    color: 'blue',
+    type: 'cap'
+  }
+]
